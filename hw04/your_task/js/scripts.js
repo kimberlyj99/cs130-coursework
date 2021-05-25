104936f811f7a8835d4bcb586f71c39fc4e55017
Kimberly Jimenez <kimberlyjimenez@Kimberlys-MacBook-Pro.local>
2021-05-25 10:37:36 -0500

    Turning in my hw04 folder

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -14,20 +14,72 @@ const search = (ev) => {
     if (ev) {
         ev.preventDefault();
     }
-}
+};
+
+const playTrack = (ev) => {
+    console.log(ev.currentTarget);
+    const elem = ev.currentTarget;
+    const previewURL = elem.dataset.previewTrack;
+    console.log(previewURL);
+    if (previewURL) {
+        audioPlayer.setAudioFile(previewURL);
+        audioPlayer.play();
+    } else {
+        console.log('there is no preview available for this track.')
+    }
+    document.querySelector('footer .track-item').innerHTML = elem.innerHTML;
+
+};
 
 const getTracks = (term) => {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+    let url = baseURL + "?type=track&q=" + term + "&limit=5";
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            document.querySelector('#tracks').innerHTML = '';
+            for (const track of data) {
+                const template = `
+                    <section class="track-item preview" data-preview-track="${track.preview_url}" onclick="playTrack(event)";>
+                        <img src="${track.album.image_url}">
+                        <i class="fas play-track fa-play" aria-hidden="true"></i>
+                    <div class="label">
+                        <h3>${track.name}</h3>
+                        <p>
+                            ${track.artist.name}
+                        </p>
+                    </div>
+                    </section>`;
+            document.querySelector('#tracks').innerHTML += template;
+            console.log(track);
+            }
+
+    })
 };
 
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    let url = baseURL + "?type=album&q=" + term;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            document.querySelector('#albums').innerHTML = '';
+            for (const album of data) {
+                const template = 
+                    `<section class="album-card" id="${album.id}">
+                        <div>
+                            <img src="${album.image_url}">
+                            <h3>${album.name}</h3>
+                            <div class="footer">
+                                <a href="${album.spotify_url}" target="_blank">
+                                    view on spotify
+                                </a>
+                            </div>
+                        </div>
+                    </section>`;
+            document.querySelector('#albums').innerHTML += template;
+            console.log(album);
+            }
+
+    })
 };
 
 const getArtist = (term) => {
